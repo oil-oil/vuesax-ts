334d9fe76e34cbbb23a4ccb2a94393d3dcff2789
rCatMan <rCatMan@outlook.com>
2023-06-13 16:56:10 +0800

    style : change git config core.ignorecase

---
+++ packages/docs/docs/.vitepress/theme/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import { defineComponent } from "vue";
+
+import NavbarLinks from "./NavbarLinks.tsx";
+import NavbarTitle from "./NavbarTitle.tsx";
+import "./style/style.scss";
+
+const NavBar = defineComponent({
+  name: "NavBar",
+  slots: ["nav-bar-title-before"],
+  setup() {
+    return () => (
+      <div class="Nav">
+        {/* <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')"/> */}
+        <NavbarTitle class="NavBarTitle" />
+        <NavbarLinks />
+        <div class="curtain"></div>
+      </div>
+    );
+  },
+});
+export default NavBar;
