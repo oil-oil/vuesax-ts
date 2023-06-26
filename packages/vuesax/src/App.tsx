f4c9a26f7f554a7b4d07afa8085a5c42ed6661b2
rCatMan <rCatMan@outlook.com>
2023-06-26 15:22:32 +0800

    fix: update Navbar test

--- packages/vuesax/src/App.tsx
+++ packages/vuesax/src/App.tsx
@@ -1,138 +1,51 @@
 import { defineComponent, ref } from "vue";
 
-import {
-  VsAvatar,
-  VsAvatarGroup,
-  VsCheckbox,
-  VsInput,
-  VsOption,
-  VsSelect,
-  VsTable,
-  VsTd,
-  VsTh,
-  VsTr,
-} from "./components";
+import { VsNavbar, VsNavbarGroup, VsNavbarItem, VsButton } from "./components";
 
 export default defineComponent({
   setup() {
-    const data = ref([
-      {
-        id: 1,
-        name: "Leanne Graham",
-        username: "Bret",
-        email: "Sincere@april.biz",
-        website: "hildegard.org",
-      },
-      {
-        id: 2,
-        name: "Ervin Howell",
-        username: "Antonette",
-        email: "Shanna@melissa.tv",
-        website: "anastasia.net",
-      },
-      {
-        id: 3,
-        name: "Clementine Bauch",
-        username: "Samantha",
-        email: "Nathan@yesenia.net",
-        website: "ramiro.info",
-      },
-      {
-        id: 4,
-        name: "Patricia Lebsack",
-        username: "Karianne",
-        email: "Julianne.OConner@kory.org",
-        website: "kale.biz",
-      },
-      {
-        id: 5,
-        name: "Chelsey Dietrich",
-        username: "Kamren",
-        email: "Lucio_Hettinger@annie.ca",
-        website: "demarco.info",
-      },
-      {
-        id: 6,
-        name: "Mrs. Dennis Schulist",
-        username: "Leopoldo_Corkery",
-        email: "Karley_Dach@jasper.info",
-        website: "ola.org",
-      },
-      {
-        id: 7,
-        name: "Kurtis Weissnat",
-        username: "Elwyn.Skiles",
-        email: "Telly.Hoeger@billy.biz",
-        website: "elvis.io",
-      },
-      {
-        id: 8,
-        name: "Nicholas Runolfsdottir V",
-        username: "Maxime_Nienow",
-        email: "Sherwood@rosamond.me",
-        website: "jacynthe.com",
-      },
-      {
-        id: 9,
-        name: "Glenna Reichert",
-        username: "Delphine",
-        email: "Chaim_McDermott@dana.io",
-        website: "conrad.com",
-      },
-      {
-        id: 10,
-        name: "Clementina DuBuque",
-        username: "Moriah.Stanton",
-        email: "Rey.Padberg@karina.biz",
-        website: "ambrose.net",
-      },
-    ]);
-
-    const search = ref("");
+    const active = ref("guide");
     return () => (
       <>
-        <VsTable
-          striped
+        <VsNavbar
+          fixed
+          textWhite
+          dark
+          square
+          paddingScroll
+          v-model={active.value}
           v-slots={{
-            header: () => (
-              <VsInput
-                v-model={search.value}
-                border
-                placeholder="Search"
-                style={{ width: "100%" }}
-              />
-            ),
-            thead: () => (
-              <VsTr>
-                <VsTh>
-                  <VsCheckbox></VsCheckbox>
-                </VsTh>
-                <VsTh sort>Name</VsTh>
-                <VsTh sort>Email</VsTh>
-                <VsTh>Id</VsTh>
-              </VsTr>
-            ),
-            tbody: () =>
-              data.value.map(({ username, email, id }) => (
-                <VsTr
-                  v-slots={{
-                    expand: () => <div style={{ width: "100%" }}>123</div>,
-                  }}
-                >
-                  <VsTd checkbox>
-                    <VsCheckbox
-                      onClick={(e) => {
-                        e.stopPropagation();
-                      }}
-                    ></VsCheckbox>
-                  </VsTd>
-                  <VsTd checkbox>{id}</VsTd>
-                  <VsTd>{username}</VsTd>
-                  <VsTd>{email}</VsTd>
-                </VsTr>
-              )),
+            left: () => <>1111</>,
+            right: () => <VsButton flat>Login</VsButton>,
           }}
-        ></VsTable>
+        >
+          <VsNavbarGroup
+            v-slots={{
+              items: () => (
+                <>
+                  <VsNavbarItem active={active.value === "baidu"} id="baidu">
+                    baidu
+                  </VsNavbarItem>
+                  <VsNavbarItem active={active.value === "tx"} id="tx">
+                    tx
+                  </VsNavbarItem>
+                </>
+              ),
+            }}
+          >
+            Docs
+          </VsNavbarGroup>
+          <VsNavbarItem active={active.value === "guide"} id="guide">
+            Guide
+          </VsNavbarItem>
+          <VsNavbarItem active={active.value === "components"} id="components">
+            Documents
+          </VsNavbarItem>
+          <VsNavbarItem active={active.value === "license"} id="license">
+            License
+          </VsNavbarItem>
+        </VsNavbar>
+        <div style={{ height: "1000px" }}></div>
       </>
     );
   },
--- packages/vuesax/src/components/navbar/base/Navbar.cy.tsx
+++ packages/vuesax/src/components/navbar/base/Navbar.cy.tsx
@@ -1,29 +1,32 @@
-import { ref } from "vue";
+import { reactive } from "vue";
 
-import { VsButton, VsNavbar, VsNavbarGroup, VsNavbarItem } from "@/components";
+import { VsNavbar, VsNavbarGroup, VsNavbarItem } from "@/components";
 
 describe("Navbar", () => {
+  const active = reactive({
+    basic: "guide",
+    click: "guide",
+  });
   it("test Navbar basic render", () => {
-    const active = ref("guide");
     cy.mount(
       <VsNavbar
         fixed
         textWhite
         primary
-        v-model={active.value}
+        v-model={active.basic}
         v-slots={{
           left: () => <>1111</>,
-          right: () => <VsButton flat>Login</VsButton>,
+          right: () => 2222,
         }}
       >
         <VsNavbarGroup
           v-slots={{
             items: () => (
               <>
-                <VsNavbarItem active={active.value === "baidu"} id="baidu">
+                <VsNavbarItem active={active.basic === "baidu"} id="baidu">
                   baidu
                 </VsNavbarItem>
-                <VsNavbarItem active={active.value === "tx"} id="tx">
+                <VsNavbarItem active={active.basic === "tx"} id="tx">
                   tx
                 </VsNavbarItem>
               </>
@@ -32,20 +35,64 @@ describe("Navbar", () => {
         >
           Docs
         </VsNavbarGroup>
-        <VsNavbarItem active={active.value === "guide"} id="guide">
+        <VsNavbarItem active={active.basic === "guide"} id="guide">
           Guide
         </VsNavbarItem>
-        <VsNavbarItem active={active.value === "components"} id="components">
+        <VsNavbarItem active={active.basic === "components"} id="components">
           Documents
         </VsNavbarItem>
-        <VsNavbarItem active={active.value === "license"} id="license">
+        <VsNavbarItem active={active.basic === "license"} id="license">
           License
         </VsNavbarItem>
       </VsNavbar>
     );
-    cy.get(".vs-navbar__right,.vs-navbar__center,.vs-navbar__left").should(
-      "be.visible"
+    cy.get(
+      ".vs-navbar__right,.vs-navbar__center,.vs-navbar__left,.vs-navbar__line"
+    ).should("be.visible");
+    // cy.contains("License").realHover().should("have.css", "opacity", "1");
+  });
+
+  it("test Navbar clickEvent", () => {
+    cy.mount(
+      <VsNavbar
+        fixed
+        textWhite
+        primary
+        v-model={active.click}
+        v-slots={{
+          left: () => <>1111</>,
+          right: () => 2222,
+        }}
+      >
+        <VsNavbarGroup
+          v-slots={{
+            items: () => (
+              <>
+                <VsNavbarItem active={active.click === "baidu"} id="baidu">
+                  baidu
+                </VsNavbarItem>
+                <VsNavbarItem active={active.click === "tx"} id="tx">
+                  tx
+                </VsNavbarItem>
+              </>
+            ),
+          }}
+        >
+          Docs
+        </VsNavbarGroup>
+        <VsNavbarItem active={active.click === "guide"} id="guide">
+          Guide
+        </VsNavbarItem>
+        <VsNavbarItem active={active.click === "components"} id="components">
+          Documents
+        </VsNavbarItem>
+        <VsNavbarItem active={active.basic === "license"} id="license">
+          License
+        </VsNavbarItem>
+      </VsNavbar>
     );
-    cy.get(".vs-navbar__line").should("be.visible");
+
+    cy.contains("Documents").click();
+    cy.wrap(active).its("click").should("eq", "components");
   });
 });
