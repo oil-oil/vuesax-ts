396e77ff9676b585edcf55120afbec9448be65b9
rCatMan <rCatMan@outlook.com>
2023-07-21 18:52:19 +0800

    feat: add Notification component test

---
+++ packages/vuesax/src/components/notification/__test__/Notification.cy.tsx
@@ -0,0 +1,47 @@
+import { ref } from "vue";
+
+import { VsNotification } from "@/components";
+
+describe("Notification", () => {
+  it("test Notification basic render", () => {
+    cy.mount(
+      <VsNotification
+        title="Documentation Vuesax 4.0+"
+        text="These documents refer to the latest version of vuesax (4.0+), to see the documents of the previous versions you can do it here 👉 Vuesax 3.x"
+      ></VsNotification>
+    );
+    cy.get(".vs-notification ").should("be.visible");
+  });
+
+  it("test notification different type", () => {
+    const typeArray = ["square", "border", "flat", "square"];
+
+    typeArray.forEach((type) => {
+      cy.mount(
+        <VsNotification
+          title="Documentation Vuesax 4.0+"
+          text="These documents refer to the latest version of vuesax (4.0+), to see the documents of the previous versions you can do it here 👉 Vuesax 3.x"
+          {...{ [type]: true }}
+        />
+      );
+      cy.get(`.vs-notification--${type}`).should("be.visible");
+    });
+  });
+
+  it("test notification Loading", () => {
+    const loading = ref(true);
+    cy.mount(<VsNotification loading={loading} />);
+    cy.get(".vs-notification--loading").should("be.visible");
+  });
+
+  it("test notification progress", () => {
+    cy.mount(
+      <VsNotification
+        progressAuto
+        title="Documentation Vuesax 4.0+"
+        text="These documents refer to the latest version of vuesax (4.0+), to see the documents of the previous versions you can do it here 👉 Vuesax 3.x"
+      />
+    );
+    cy.get(".vs-notification__progress").should("be.visible");
+  });
+});
