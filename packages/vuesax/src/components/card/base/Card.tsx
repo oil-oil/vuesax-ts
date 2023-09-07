ed90ee016b66281a579b9b084c0918cb92dec27d
oil欧呦 <2228586315@qq.com>
2023-09-08 00:10:29 +0800

    fix: lint code

--- packages/vuesax/src/components/card/base/Card.tsx
+++ packages/vuesax/src/components/card/base/Card.tsx
@@ -18,28 +18,32 @@ const Card = defineComponent({
   setup(props, { slots, attrs }) {
     const cardAttrs = attrs as HTMLAttributes
 
-
-
     return () => (
-      <div class={['vs-card-content', `type-${props.type}`]}><div class="vs-card" {...cardAttrs}>
-        {slots.img && (
-          <div class="vs-card__img">
-            {slots.img?.()}
-            {slots.interactions && (
-              <div class="vs-card__interactions">{slots.interactions?.()}</div>
-            )}
-          </div>
-        )}
-        {slots.text && (
-          <div class="vs-card__text">
-            {slots.title && <div class="vs-card__title">{slots.title?.()}</div>}
-            {slots.text?.()}
-          </div>
-        )}
-        {slots.button && (
-          <div class="vs-card__buttons">{slots.buttons?.()}</div>
-        )}
-      </div></div>
+      <div class={['vs-card-content', `type-${props.type}`]}>
+        <div class="vs-card" {...cardAttrs}>
+          {slots.img && (
+            <div class="vs-card__img">
+              {slots.img?.()}
+              {slots.interactions && (
+                <div class="vs-card__interactions">
+                  {slots.interactions?.()}
+                </div>
+              )}
+            </div>
+          )}
+          {slots.text && (
+            <div class="vs-card__text">
+              {slots.title && (
+                <div class="vs-card__title">{slots.title?.()}</div>
+              )}
+              {slots.text?.()}
+            </div>
+          )}
+          {slots.button && (
+            <div class="vs-card__buttons">{slots.buttons?.()}</div>
+          )}
+        </div>
+      </div>
     )
   }
 })
